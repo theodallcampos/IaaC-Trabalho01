42fc0e01a7938f036bd0a073b7e3f1dc87bf0e8b
theodallcampos <100042068+theodallcampos@users.noreply.github.com>
2022-11-18 11:55:44 -0300

    Update main.tf

--- terraform-jenkins/modules/vpc/main.tf
+++ terraform-jenkins/modules/vpc/main.tf
@@ -1,1 +1,19 @@
 
+# Variable that holds the CIDR block for the VPC
+variable "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+}
+
+# Creting the VPC and calling it tutorial_vpc
+resource "aws_vpc" "tutorial_vpc" {
+  # Setting the CIDR block of the VPC to the variable vpc_cidr_block
+  cidr_block = var.vpc_cidr_block
+
+  # Enabling DNS hostnames on the VPC
+  enable_dns_hostnames = true
+
+  # Setting the tag Name to tutorial_vpc
+  tags = {
+    Name = "tutorial_vpc"
+  }
+}
