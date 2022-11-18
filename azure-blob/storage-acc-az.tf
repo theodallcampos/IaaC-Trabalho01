f5b0bae068f6f538f4f37fc4d19a155f596bbe38
theodallcampos <100042068+theodallcampos@users.noreply.github.com>
2022-11-18 09:31:27 -0300

    Create storage-acc-az.tf

---
+++ azure-blob/storage-acc-az.tf
@@ -0,0 +1,7 @@
+resource "azurerm_storage_account" "mack" {
+  name                     = "mackiaac"
+  resource_group_name      = azurerm_resource_group.mack.name
+  location                 = azurerm_resource_group.mack.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
