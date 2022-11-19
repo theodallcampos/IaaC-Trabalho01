5b5d7fce69a8cabae7d44a4360e70754b4fea9a7
theodallcampos <100042068+theodallcampos@users.noreply.github.com>
2022-11-18 23:21:46 -0300

    Update main.tf

--- aws-rundeck/main.tf
+++ aws-rundeck/main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIA2EGQM65IRH57W2W4"
-  secret_key = "ZlKpvk6cwM5mojvonFQO8AxuVlPUIaVyt7eMM8l3XXXXsayt"
-}
-
 resource "aws_instance" "vm03" {
   ami = "ami-0885b1f6bd170450c"
   instance_type = "t2.micro"
