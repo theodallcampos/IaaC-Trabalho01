772c776cc0c1226fe14c73e46ff9051e9e6a62e4
theodallcampos <100042068+theodallcampos@users.noreply.github.com>
2022-11-18 21:41:33 -0300

    Delete terraform-jenkins directory

--- terraform-jenkins/modules/compute/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "public_ip" {
-   description = "The public IP address of the Jenkins server"
-   value = aws_eip.jenkins_eip.public_ip
-}
--- terraform-jenkins/modules/security_group/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "sg_id" {
-   description = "The ID of the security group"
-   value = aws_security_group.tutorial_jenkins_sg.id
-}
