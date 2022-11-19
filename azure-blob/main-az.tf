382aff43ba72e099a452861d9f97ee9284789694
theodallcampos <100042068+theodallcampos@users.noreply.github.com>
2022-11-18 23:28:16 -0300

    Update main-az.tf

--- azure-blob/main-az.tf
+++ azure-blob/main-az.tf
@@ -11,9 +11,9 @@ terraform {
 provider "azurerm" {
   features {}
   
-  subscription_id = "3dd4a6ef-e800-4ae1-9c13-17b3e7cb1279"
-  client_id       = "9afe694c-d375-4e9a-b238-c9986e9daf0e" 
-  client_secret   = "0bf8b0af-fa08-4922-ae13-d044325b95be"
-  tenant_id       = "dc322dbe-8018-48a9-8afc-0140f5846f50"
+  subscription_id = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+  client_id       = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX" 
+  client_secret   = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+  tenant_id       = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
 
 }
