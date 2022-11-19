560d30277e31e1b4ae93d4ddb75ed3512ae3e32f
theodallcampos <100042068+theodallcampos@users.noreply.github.com>
2022-11-18 23:57:58 -0300

    Update main.tf

--- aws-php-apache-mysql/main.tf
+++ aws-php-apache-mysql/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "vm01" {
+  resource "aws_instance" "vm01" {
   ami = "ami-0885b1f6bd170450c"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.websg.id ]
@@ -15,7 +15,7 @@ resource "aws_instance" "vm01" {
     tags = {
       Name = "VM01"
     }
-  
+  } 
   resource "aws_security_group" "websg" {
   name = "web-sg01"
   ingress {
