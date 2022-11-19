a155297bced063fc1bf1a198a3f9c919aa885719
theodallcampos <100042068+theodallcampos@users.noreply.github.com>
2022-11-18 23:59:15 -0300

    Update main.tf

--- aws-rundeck/main.tf
+++ aws-rundeck/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "vm03" {
+  resource "aws_instance" "vm03" {
   ami = "ami-0885b1f6bd170450c"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.websg.id ]
@@ -13,12 +13,12 @@ resource "aws_instance" "vm03" {
     }
   
 }
-resource "aws_security_group" "websg" {
+  resource "aws_security_group" "websg" {
   name = "web-sg03"
   ingress {
-    protocol = "tcp"
-    from_port = 0
-    to_port = 65535
-    cidr_blocks = [ "0.0.0.0/0" ]
+  protocol = "tcp"
+  from_port = 0
+  to_port = 65535
+  cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
